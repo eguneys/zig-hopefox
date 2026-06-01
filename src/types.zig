4f67b3a12ed239932ef06f4a9a28dc9fe37d010d
eguneys <eguneys@gmail.com>
2026-06-02 00:28:49 +0300

    printing

--- src/types.zig
+++ src/types.zig
@@ -72,12 +72,16 @@ const Square = enum(u8) {
     G8,
     H8,
 
-    pub fn file(self: Square) File {
+    pub fn toFile(self: Square) File {
         return @enumFromInt(@intFromEnum(self) % 8);
     }
 
-    pub fn rank(self: Square) Rank {
+    pub fn toRank(self: Square) Rank {
         return @enumFromInt(@intFromEnum(self) / 8);
+    }
+
+    pub fn fromCoord(file: File, rank: Rank) Square {
+        return @enumFromInt(@intFromEnum(file) + @intFromEnum(rank) * 8);
     }
 };
 
@@ -104,6 +108,8 @@ const Piece = enum(u8) {
 const Bitboard = struct {
     bits: u64,
 
+    pub const Zero = Bitboard{ .bits = 0 };
+
     pub fn fromSquare(sq: Square) Bitboard {
         return Bitboard{ .bits = @as(u64, 1) << @intCast(@intFromEnum(sq)) };
     }
@@ -112,46 +118,132 @@ const Bitboard = struct {
         return self.bits == other.bits;
     }
 
-    pub fn square(self: Bitboard) ?Square {
+    pub fn single(self: Bitboard) ?Square {
         return if (self.bits & (self.bits -% 1) == 0)
             @enumFromInt(@ctz(self.bits))
         else
             null;
     }
+
+    pub fn has(self: Bitboard, square: Square) bool {
+        return self.bitand(Bitboard.fromSquare(square)).isNotEmpty();
+    }
+
+    pub fn isEmpty(self: Bitboard) bool {
+        return self.bits == 0;
+    }
+
+    pub fn isNotEmpty(self: Bitboard) bool {
+        return self.bits != 0;
+    }
+
+    pub fn bitand(self: Bitboard, other: Bitboard) Bitboard {
+        return Bitboard{ .bits = self.bits & other.bits };
+    }
 };
 
 test "square" {
-    try std.testing.expect(Square.A1.file() == File.A);
-    try std.testing.expect(Square.B1.file() == File.B);
-    try std.testing.expect(Square.C1.file() == File.C);
-    try std.testing.expect(Square.D1.file() == File.D);
-    try std.testing.expect(Square.E1.file() == File.E);
-    try std.testing.expect(Square.F1.file() == File.F);
-    try std.testing.expect(Square.G1.file() == File.G);
-    try std.testing.expect(Square.H1.file() == File.H);
+    try std.testing.expect(Square.A1.toFile() == File.A);
+    try std.testing.expect(Square.B1.toFile() == File.B);
+    try std.testing.expect(Square.C1.toFile() == File.C);
+    try std.testing.expect(Square.D1.toFile() == File.D);
+    try std.testing.expect(Square.E1.toFile() == File.E);
+    try std.testing.expect(Square.F1.toFile() == File.F);
+    try std.testing.expect(Square.G1.toFile() == File.G);
+    try std.testing.expect(Square.H1.toFile() == File.H);
 
-    try std.testing.expect(Square.A1.rank() == Rank.R1);
-    try std.testing.expect(Square.B1.rank() == Rank.R1);
-    try std.testing.expect(Square.C1.rank() == Rank.R1);
-    try std.testing.expect(Square.D1.rank() == Rank.R1);
-    try std.testing.expect(Square.E1.rank() == Rank.R1);
-    try std.testing.expect(Square.F1.rank() == Rank.R1);
-    try std.testing.expect(Square.G1.rank() == Rank.R1);
-    try std.testing.expect(Square.H1.rank() == Rank.R1);
+    try std.testing.expect(Square.A1.toRank() == Rank.R1);
+    try std.testing.expect(Square.B1.toRank() == Rank.R1);
+    try std.testing.expect(Square.C1.toRank() == Rank.R1);
+    try std.testing.expect(Square.D1.toRank() == Rank.R1);
+    try std.testing.expect(Square.E1.toRank() == Rank.R1);
+    try std.testing.expect(Square.F1.toRank() == Rank.R1);
+    try std.testing.expect(Square.G1.toRank() == Rank.R1);
+    try std.testing.expect(Square.H1.toRank() == Rank.R1);
 
-    try std.testing.expect(Square.A2.rank() == Rank.R2);
-    try std.testing.expect(Square.A3.rank() == Rank.R3);
-    try std.testing.expect(Square.A4.rank() == Rank.R4);
-    try std.testing.expect(Square.A5.rank() == Rank.R5);
-    try std.testing.expect(Square.A6.rank() == Rank.R6);
-    try std.testing.expect(Square.A7.rank() == Rank.R7);
-    try std.testing.expect(Square.A8.rank() == Rank.R8);
+    try std.testing.expect(Square.A2.toRank() == Rank.R2);
+    try std.testing.expect(Square.A3.toRank() == Rank.R3);
+    try std.testing.expect(Square.A4.toRank() == Rank.R4);
+    try std.testing.expect(Square.A5.toRank() == Rank.R5);
+    try std.testing.expect(Square.A6.toRank() == Rank.R6);
+    try std.testing.expect(Square.A7.toRank() == Rank.R7);
+    try std.testing.expect(Square.A8.toRank() == Rank.R8);
 
-    try std.testing.expect(Bitboard.fromSquare(Square.A1).square() == Square.A1);
+    try std.testing.expect(Bitboard.fromSquare(Square.A1).single() == Square.A1);
 
-    try std.testing.expect(Bitboard.fromSquare(Square.A1).square() == Square.A1);
-    try std.testing.expect(Bitboard.fromSquare(Square.H8).square() == Square.H8);
+    try std.testing.expect(Bitboard.fromSquare(Square.A1).single() == Square.A1);
+    try std.testing.expect(Bitboard.fromSquare(Square.H8).single() == Square.H8);
 
     for (Squares) |square|
-        try std.testing.expect(Bitboard.fromSquare(square).square() == square);
+        try std.testing.expect(Bitboard.fromSquare(square).single() == square);
+}
+
+const Direction = enum { Up, Down, Left, Right, Up_Left, Up_Right, Down_Left, Down_Right };
+
+const Attacks = struct {
+    const ray_masks = generate_ray_masks();
+
+    fn generate_ray_masks() Bitboard[8][64] {
+        var res = undefined;
+
+        const df = [8]u8{ 0, 0, -1, 1, -1, 1, -1, 1 };
+        const dr = [8]u8{ 1, -1, 0, 0, 1, 1, -1, -1 };
+
+        for (Squares) |square| {
+            const start_file = square.file();
+            const start_rank = square.rank();
+
+            for (0..8) |dir| {
+                var mask = 0;
+                var f = start_file + df[dir];
+                var r = start_rank + dr[dir];
+
+                while (f >= 0 and f < 8 and r >= 0 and r < 8) {
+                    const target_square = Square.fromCoord(f, r);
+                    mask |= Bitboard.fromSquare(target_square);
+
+                    f += df[dir];
+                    r += dr[dir];
+                }
+
+                res[dir][square] = mask;
+            }
+        }
+        return res;
+    }
+
+    fn ray_attacks(square: Square, occupied: Bitboard) Bitboard {
+        return ray_masks[square] & occupied;
+    }
+};
+
+const Prints = struct {
+    pub fn bitboard(self: Bitboard) [71]u8 {
+        var string: [71]u8 = undefined;
+        for (&string, 0..) |*val, i| {
+            const f: usize = i % 9;
+            const r: usize = (71 - i) / 9;
+            if (f == 8) {
+                val.* = '\n';
+            } else if (self.has(Square.fromCoord(@enumFromInt(f), @enumFromInt(r)))) {
+                val.* = '+';
+            } else {
+                val.* = '.';
+            }
+        }
+        return string;
+    }
+};
+
+test "bitboards" {
+    try std.testing.expectEqualStrings(&Prints.bitboard(Bitboard.Zero),
+        \\........
+        \\........
+        \\........
+        \\........
+        \\........
+        \\........
+        \\........
+        \\........
+    );
 }
