3599550658af8412938e7668f4a7ae2806c6ebda
Emre Guneyler <eguneys@gmail.com>
2026-06-04 01:36:26 +0300

    ok

--- src/gof/parser.zig
+++ src/gof/parser.zig
@@ -551,14 +551,45 @@ test "parser definition" {
     try std.testing.expectEqualStrings("Captured", program.blocks[0].definitions[0].calls[0].arguments[2].value);
 }
 
-test "fuzz example" {
-    const global = struct {
-        fn testOne(ctx: type, input: *std.testing.Smith) anyerror!void {
-            _ = ctx;
-            var output = [_]u8{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-            input.bytes(&output);
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", &output));
-        }
+test "fuzz lexer" {
+    try std.testing.fuzz({}, fuzzLexer, .{});
+}
+
+fn fuzzLexer(context: void, smith: *std.testing.Smith) !void {
+    _ = context;
+
+    _ = smith;
+    //const a = smith.value(u8);
+    //try std.testing.expect(a != 3);
+}
+
+fn testOne(context: void, smith: *std.testing.Smith) !void {
+    _ = context;
+    // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
+
+    const gpa = std.testing.allocator;
+    var list: std.ArrayList(u8) = .empty;
+    defer list.deinit(gpa);
+    while (!smith.eos()) switch (smith.value(enum { add_data, dup_data })) {
+        .add_data => {
+            const slice = try list.addManyAsSlice(gpa, smith.value(u4));
+            smith.bytes(slice);
+        },
+        .dup_data => {
+            if (list.items.len == 0) continue;
+            if (list.items.len > std.math.maxInt(u32)) return error.SkipZigTest;
+            const len = smith.valueRangeAtMost(u32, 1, @min(32, list.items.len));
+            const off = smith.valueRangeAtMost(u32, 0, @intCast(list.items.len - len));
+            try list.appendSlice(gpa, list.items[off..][0..len]);
+            try std.testing.expectEqualSlices(
+                u8,
+                list.items[off..][0..len],
+                list.items[list.items.len - len ..],
+            );
+        },
     };
-    try std.testing.fuzz(global, global.testOne, .{});
+}
+
+test "fuzz example" {
+    //try std.testing.fuzz({}, testOne, .{});
 }
