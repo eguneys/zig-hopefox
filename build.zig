b6f91dace7015203a3580331013d548fd2af8d8d
eguneys <eguneys@gmail.com>
2026-06-04 02:03:07 +0300

    format

--- build.zig
+++ build.zig
@@ -29,10 +29,6 @@ pub fn build(b: *std.Build) void {
 
     run_cmd.step.dependOn(b.getInstallStep());
 
-    //if (b.args) |args| {
-    //run_cmd.addArgs(args);
-    //}
-
     const mod_tests = b.addTest(.{
         .root_module = mod,
     });
