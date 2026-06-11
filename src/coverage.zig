ec24bb7796d22354e25798c990fdbb1fff729897
eguneys <eguneys@gmail.com>
2026-06-11 15:56:14 +0300

    scripts running on 1000 positions

--- src/coverage.zig
+++ src/coverage.zig
@@ -6,6 +6,8 @@ const DbReader = @import("db_file.zig").DbReader;
 pub const DotCoverageOutput = struct {
     pub fn write(allocator: std.mem.Allocator, db_reader: *DbReader, dot: *DotUsage, writer: *std.Io.File.Writer) !void {
         for (0..db_reader.header.count) |i| {
+            if (i > 0) _ = try writer.interface.write("\n");
+
             const position = try db_reader.readPosition(i);
             //const meta = try db_reader.readMeta(i);
             //const meta_id: [5]u8 = @bitCast(meta.id);
@@ -20,8 +22,6 @@ pub const DotCoverageOutput = struct {
                 return;
             };
             _ = try writer.interface.write(output);
-
-            _ = try writer.interface.write("\n");
         }
     }
 };
